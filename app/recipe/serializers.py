adfc44bd23eb3535a696185aa342b8f8968a3373
Renat Khusainov <renat.khusainov@gmail.com>
2023-06-17 17:00:37 +0300

    0:00 / 6:11 85. Write tests for creating recipes [Follow Along]

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -15,4 +15,9 @@ class RecipeSerializer(serializers.ModelSerializer):
 		fields = ['id', 'title', 'time_minutes', 'price', 'link']
 		read_only_fields = ['id']
 		
+class RecipeDetailSerializer(RecipeSerializer):
+	"""Seriliazer for recipe detail view."""
+	
+	class Meta(RecipeSerializer.Meta):
+		fields = RecipeSerializer.Meta.fields + ['description']
 		
